0fbfe74540303b382dd91334643d877342b6b95f
Shirisha Reddy <114852869+shirisha-1910@users.noreply.github.com>
2024-07-12 12:36:37 +0530

    Update image.sh

--- image.sh
+++ image.sh
@@ -4,8 +4,8 @@
 docker pull sirishassss/new-temp:latest
 
 # Stop any existing containers running the previous version
-docker stop new-temp-container || true
-docker rm new-temp-container || true
+docker stop new-temp || true
+docker rm new-temp || true
 
 # Run a new container with the updated image
 docker run -d --name new-temp-container -p 8080:80 sirishassss/new-temp:latest
