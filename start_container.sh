a1e730f507b8154b7247990810c6b55071902247
shirisha-1910 <shirishareddy.yasa@gmail.com>
2024-09-04 16:47:15 +0530

    cmd modify

--- start_container.sh
+++ start_container.sh
@@ -3,8 +3,9 @@ set -e
 
 # Install AWS CLI if not already installed
 if ! command -v aws &> /dev/null; then
-    echo "AWS CLI not found, installing..."
-    sudo yum install -y aws-cli  # For Amazon Linux, RHEL, CentOS
+    echo "AWS CLI not found, installing..."sudo apt-get update
+    sudo apt-get update
+    sudo apt-get install -y aws-cli  # For Amazon Linux, RHEL, CentOS
     # For other distros, adjust the installation command accordingly
 fi
 
