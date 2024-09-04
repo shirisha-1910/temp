a9bc2929b2c25aae376fe6065b97a073018434a7
shirisha-1910 <shirishareddy.yasa@gmail.com>
2024-09-04 17:43:59 +0530

    modification

--- start_container.sh
+++ start_container.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
 set -e
 
-# Update package lists
-echo "Updating package lists..."
-sudo apt-get update
-
-# Install required packages
-echo "Installing required packages..."
-sudo apt-get install -y unzip curl
-
-# Download the AWS CLI version 2 package
-echo "Downloading AWS CLI version 2..."
-curl "https://d1uj6qtbmh3dt5.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-
-# Unzip the package
-echo "Unzipping AWS CLI version 2 package..."
-unzip awscliv2.zip
-
-# Install AWS CLI version 2
-echo "Installing AWS CLI version 2..."
-sudo ./aws/install
-
-# Verify installation
-echo "Verifying AWS CLI installation..."
-aws --version
-
 # Set the region and repository URL
 REGION="ap-south-1"
 REPO_URL="905418202800.dkr.ecr.${REGION}.amazonaws.com/temp:latest"
