4e30ba0fed8598dc5c75f191d33253ad41e53970
shirisha-1910 <shirishareddy.yasa@gmail.com>
2024-09-04 16:39:01 +0530

    modifation

--- start_container.sh
+++ start_container.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from  ECR
-docker pull 905418202800.dkr.ecr.ap-south-1.amazonaws.com/temp:latest
+# Set the region and repository URL
+REGION="ap-south-1"
+REPO_URL="905418202800.dkr.ecr.${REGION}.amazonaws.com/temp:latest"
+
+# Authenticate Docker to the ECR registry
+echo "Authenticating Docker to ECR..."
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin 905418202800.dkr.ecr.${REGION}.amazonaws.com
+
+# Pull the Docker image from ECR
+echo "Pulling Docker image from ECR..."
+docker pull $REPO_URL
 
 # Run the Docker image as a container
-docker run -d -p 8000:8000 905418202800.dkr.ecr.ap-south-1.amazonaws.com/temp:latest
+echo "Running Docker container..."
+docker run -d -p 8000:8000 $REPO_URL
