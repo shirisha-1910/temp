06a33a67ed8852a5a5baa90d3d10692e61d281c4
shirisha-1910 <shirishareddy.yasa@gmail.com>
2024-09-04 16:17:19 +0530

    start-container

--- start_container.sh
+++ start_container.sh
@@ -1,30 +1,8 @@
 #!/bin/bash
 set -e
 
-# Define your ECR repository and image tag
-ECR_REPO_URI="905418202800.dkr.ecr.ap-south-1.amazonaws.com/temp"
-IMAGE_TAG="latest"  # Specify the image tag you want to use
-IMAGE_NAME="$ECR_REPO_URI:$IMAGE_TAG"
-PORT=8000
+# Pull the Docker image from  ECR
+docker pull 905418202800.dkr.ecr.ap-south-1.amazonaws.com/temp:latest
 
-# Check if AWS CLI is installed
-if ! command -v aws &> /dev/null; then
-  echo "AWS CLI could not be found. Please install AWS CLI."
-  exit 1
-fi
-
-# Authenticate Docker to your AWS ECR registry
-echo "Authenticating Docker to AWS ECR..."
-if ! aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin $ECR_REPO_URI; then
-  echo "Failed to authenticate Docker to AWS ECR."
-  exit 1
-fi
-
-# Start the Docker container
-echo "Starting Docker container..."
-if ! sudo docker run -d -p $PORT:8000 --name my_container $IMAGE_NAME; then
-  echo "Failed to start Docker container."
-  exit 1
-fi
-
-echo "Docker container started successfully."
+# Run the Docker image as a container
+docker run -d -p 8000:8000 905418202800.dkr.ecr.ap-south-1.amazonaws.com/temp:latest
